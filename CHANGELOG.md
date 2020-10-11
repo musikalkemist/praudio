632f46ac309023df6bf9ef43f13b36ec91a57986
Valerio <velardovalerio@gmail.com>
2020-10-11 19:12:48 +0200

    Laid out repo structure.

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# CHANGELOG
+This file provides a history of the changes in the repo.
+
+# [0.0.1] - 11.10.20 - Valerio Velardo
+- Laid out repo structure
---
+++ setup.py
@@ -0,0 +1,36 @@
+import setuptools
+from importlib.machinery import SourceFileLoader
+
+version = SourceFileLoader("praudio.version",
+                           "praudio/version.py").load_module()
+
+with open("requirements.txt", "r") as fp:
+    required = fp.read().splitlines()
+
+setuptools.setup(
+    name="praudio",
+    version=version.__version__,
+    author="Valerio Velardo",
+    author_email="velardovalerio@gmail.com",
+    description="Audio preprocessing framework for Deep Learning audio "
+                "applications",
+    url="https://github.com/musikalkemist/praudio",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    project_urls={
+        "Source": "https://github.com/musikalkemist/praudio",
+        "Tracker": "https://github.com/musikalkemist/praudio/issues",
+    },
+    python_requires=">=3.7",
+    install_requires=required,
+    extras_require={
+        "testing": [
+            "pytest",
+            "coverage",
+            "pytest-mock"
+        ]
+    }
+)
